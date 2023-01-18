47d349b34ad23700438ac8b70904f199b9eec132
Anton <>
2023-01-18 10:28:34 +0100

    Add project files.

---
+++ Uppgift 1.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Triangel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("   /\\ ");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+
+
+        }
+    }
+}
